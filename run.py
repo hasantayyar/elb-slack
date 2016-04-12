f24652ab37481bc6f0628e118d45d7887bdc6539
Hasan Tayyar BESIK <tayyar.besik@gmail.com>
2016-04-12 22:47:45 +0300

    bootstrap of project

--- run.py
+++ run.py
@@ -3,18 +3,19 @@
 import sys
 import logging
 import logging.config
-import aws
+import elb
 
 
 def main():
     kwargs = {
         'format': '[%(asctime)s] %(message)s',
         'datefmt': '%m/%d/%Y %H:%M:%S',
-        'level': logging.DEBUG if settings.DEBUG else logging.INFO,
+        'level': logging.DEBUG,
         'stream': sys.stdout,
     }
     logging.basicConfig(**kwargs)
     logging.getLogger('requests.packages.urllib3.connectionpool').setLevel(logging.WARNING)
+    elb.get_instances("facedetect-worker")
 
 
 if __name__ == '__main__':
